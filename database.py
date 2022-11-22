f96f5192a4d8c7da71f101085dd1c31503277e3a
waelsofttun <wsboxtunisia@gmail.com>
2022-11-22 11:18:46 +0100

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://user_name:password@endpoint/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql://wael:aws2022*@database-1.cyxvrlvkxpnt.us-east-1.rds.amazonaws.com/postgres"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
